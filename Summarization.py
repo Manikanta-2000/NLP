a2ca680eb9b9dd36dff8ff1751875d522e0f958b
Manikanta-2000 <manikantaganti@gmail.com>
2023-12-02 01:58:34 -0600

    Final Commit

--- Summarization.py
+++ Summarization.py
@@ -5,7 +5,11 @@
 
 # In[ ]:
 
+#References - 1)https://stackabuse.com/text-summarization-with-nltk-in-python/
 
+#2)https://www.mygreatlearning.com/blog/text-summarization-in-python/ From the above pages logic has been build for exractive text summarization
+
+#https://www.kaggle.com/code/vshantam/text-summarization-extractive-bleu/input From the above link articles and their summaries were taken for validation The folder BBC News Summary was donwloaded from above link
 
 
 
